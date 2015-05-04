25f03d4fd97326af2b9287a3d134a17a28eb7c62
Noitidart <noitidart@gmail.com>
2015-05-04 08:37:48 -0700

    initial.rev11
    testing timings

--- modules/workers/MainWorker.js
+++ modules/workers/MainWorker.js
@@ -42,7 +42,7 @@ self.addEventListener('message', msg => worker.handleMessage(msg));
 ////// end of imports and definitions
 
 function init(objCore) {
-	console.log('in worker init');
+	//console.log('in worker init');
 	
 	// merge objCore into core
 	// core and objCore is object with main keys, the sub props
@@ -102,9 +102,9 @@ function shootSect(c1, c2) {
 				var cStr = ostypes.TYPE.LPCTSTR.targetType.array()('DISPLAY');
 				
 				var hdcScreen = ostypes.API('CreateDC')(cStr, null, null, null);
-				console.info('hdcScreen:', hdcScreen.toString(), uneval(hdcScreen), cutils.jscGetDeepest(hdcScreen));
+				//console.info('hdcScreen:', hdcScreen.toString(), uneval(hdcScreen), cutils.jscGetDeepest(hdcScreen));
 				if (ctypes.winLastError != 0) {
-					console.error('Failed hdcScreen, winLastError:', ctypes.winLastError);
+					//console.error('Failed hdcScreen, winLastError:', ctypes.winLastError);
 					throw new Error({
 						name: 'os-api-error',
 						message: 'Failed hdcScreen, winLastError: "' + ctypes.winLastError + '" and hdcScreen: "' + hdcScreen.toString(),
@@ -116,16 +116,16 @@ function shootSect(c1, c2) {
 				var nHeight = parseInt(cutils.jscGetDeepest(ostypes.API('GetDeviceCaps')(hdcScreen, ostypes.CONST.VERTRES)));
 				var nBPP = parseInt(cutils.jscGetDeepest(ostypes.API('GetDeviceCaps')(hdcScreen, ostypes.CONST.BITSPIXEL)));
 				
-				console.info('nWidth:', nWidth, 'nHeight:', nHeight, 'nBPP:', nBPP);
+				//console.info('nWidth:', nWidth, 'nHeight:', nHeight, 'nBPP:', nBPP);
 				
 				// force fullscreen
 				c1 = {x:0, y:0};
 				c2 = {x:nWidth, y:nHeight};
 				
 				var hdcMemoryDC = ostypes.API('CreateCompatibleDC')(hdcScreen); 
-				console.info('hdcMemoryDC:', hdcMemoryDC.toString(), uneval(hdcMemoryDC), cutils.jscGetDeepest(hdcMemoryDC));
+				//console.info('hdcMemoryDC:', hdcMemoryDC.toString(), uneval(hdcMemoryDC), cutils.jscGetDeepest(hdcMemoryDC));
 				if (ctypes.winLastError != 0) {
-					console.error('Failed hdcMemoryDC, winLastError:', ctypes.winLastError);
+					//console.error('Failed hdcMemoryDC, winLastError:', ctypes.winLastError);
 					throw new Error({
 						name: 'os-api-error',
 						message: 'Failed hdcMemoryDC, winLastError: "' + ctypes.winLastError + '" and hdcMemoryDC: "' + hdcMemoryDC.toString(),
@@ -143,16 +143,16 @@ function shootSect(c1, c2) {
 				bmi.bmiHeader.biCompression = ostypes.CONST.BI_RGB;
 				//bmi.bmiHeader.biSizeImage = nHeight * nWidth * (nBPP / 8);
 				
-				console.info('bmi:', bmi.toString());
+				//console.info('bmi:', bmi.toString());
 				
 				var pixelBuffer = ostypes.TYPE.BYTE.ptr();
-				//console.info('PRE pixelBuffer:', pixelBuffer.toString(), 'pixelBuffer.addr:', pixelBuffer.address().toString());
+				////console.info('PRE pixelBuffer:', pixelBuffer.toString(), 'pixelBuffer.addr:', pixelBuffer.address().toString());
 				// CreateDIBSection stuff
 				
 				var hbmp = ostypes.API('CreateDIBSection')(hdcMemoryDC, bmi.address(), ostypes.CONST.DIB_RGB_COLORS, pixelBuffer.address(), null, 0); 
-				console.info('hbmp:', hbmp.toString(), uneval(hbmp), cutils.jscGetDeepest(hbmp));
+				//console.info('hbmp:', hbmp.toString(), uneval(hbmp), cutils.jscGetDeepest(hbmp));
 				if (ctypes.winLastError != 0) {
-					console.error('Failed hbmp, winLastError:', ctypes.winLastError);
+					//console.error('Failed hbmp, winLastError:', ctypes.winLastError);
 					throw new Error({
 						name: 'os-api-error',
 						message: 'Failed hbmp, winLastError: "' + ctypes.winLastError + '" and hbmp: "' + hbmp.toString(),
@@ -161,9 +161,9 @@ function shootSect(c1, c2) {
 				}
 				
 				var rez_SO = ostypes.API('SelectObject')(hdcMemoryDC, hbmp);
-				console.info('rez_SO:', rez_SO.toString(), uneval(rez_SO), cutils.jscGetDeepest(rez_SO));
+				//console.info('rez_SO:', rez_SO.toString(), uneval(rez_SO), cutils.jscGetDeepest(rez_SO));
 				if (ctypes.winLastError != 0) {
-					console.error('Failed rez_SO, winLastError:', ctypes.winLastError);
+					//console.error('Failed rez_SO, winLastError:', ctypes.winLastError);
 					throw new Error({
 						name: 'os-api-error',
 						message: 'Failed rez_SO, winLastError: "' + ctypes.winLastError + '" and rez_SO: "' + rez_SO.toString(),
@@ -175,9 +175,9 @@ function shootSect(c1, c2) {
 				var h = c2.y - c1.y;
 				
 				var rez_BB = ostypes.API('BitBlt')(hdcMemoryDC, 0,0, w, h, hdcScreen, c1.x, c1.y, ostypes.CONST.SRCCOPY);
-				console.info('rez_BB:', rez_BB.toString(), uneval(rez_BB), cutils.jscGetDeepest(rez_BB));
+				//console.info('rez_BB:', rez_BB.toString(), uneval(rez_BB), cutils.jscGetDeepest(rez_BB));
 				if (ctypes.winLastError != 0) {
-					console.error('Failed rez_BB, winLastError:', ctypes.winLastError);
+					//console.error('Failed rez_BB, winLastError:', ctypes.winLastError);
 					throw new Error({
 						name: 'os-api-error',
 						message: 'Failed rez_BB, winLastError: "' + ctypes.winLastError + '" and rez_BB: "' + rez_BB.toString(),
@@ -185,90 +185,49 @@ function shootSect(c1, c2) {
 					});
 				}
 				
-				var casted = ctypes.cast(pixelBuffer, ostypes.TYPE.BYTE.array(nWidth * nHeight * 4).ptr).contents;
-				//console.info('casted:', casted.toString().replace(/ctypes\.UInt64\("0"\), /g, ''));
-				logit(casted.toString().replace(/ctypes\.UInt64\("0"\), /g, ''));
+				var casted = ctypes.cast(pixelBuffer, ostypes.TYPE.BYTE.array(w * h * 4).ptr).contents;
+				////console.info('casted:', casted.toString().replace(/ctypes\.UInt64\("0"\), /g, ''));
+				//logit(casted.toString().replace(/ctypes\.UInt64\("0"\), /g, ''));
 				
 				
 				var imagedata = new ImageData(w, h);
-				/*
-				// uint32 way
-				console.time('uint32 way');
-				var buf = new ArrayBuffer(w * h * 4);
-				var buf8 = new Uint8ClampedArray(buf);
-				var buf32 = new Uint32Array(buf);
-				
-				for (var nRow=0; nRow<h; ++nRow) {
-					for (var nCol=0; nCol<w; ++nCol) {
-						var nIndex = nRow * w + nCol;
-						var rez_byte = casted[nIndex];
-						var input = parseInt(cutils.jscGetDeepest(rez_byte));
-						
-						buf32[nIndex] = input;
-						//break;
-					}
-					//break;
-				}
-				//console.info('normalArr:', normalArr.toString());
-				imagedata.data.set(buf8);
-				console.timeEnd('uint32 way');
-				// uint32 way
-				*/
 				
 				/*
+				// straight feed way
+				console.time('straight feed');
+				imagedata.data.set(casted); // its BRG so color is jacked up
+				console.timeEnd('straight feed');
+				//*/
+				
+				///*
 				// normal way
 				console.time('normal way');
-				var normalArr = [];							
-				for (var nRow=0; nRow<h; ++nRow) {
-					for (var nCol=0; nCol<w; ++nCol) {
-						var nIndex = nRow * w + nCol;
-						var rez_byte = parseInt(casted[nIndex].toString());						
-						var input = rez_byte;
-						
-						var b =  input       & 0xff;
-						var g = (input >> 8) & 0xff;
-						var r = (input >>16) & 0xff;
-						var a = 255;
-
-						normalArr.push(r);
-						normalArr.push(g);
-						normalArr.push(b);
-						normalArr.push(a);
-						//console.log(nRow, nCol, [r, g, b]);
-						
-						//break;
-					}
+				var normalArr = [];
+				for (var nIndex=0; nIndex<casted.length; nIndex=nIndex+4) {
+					
+					var r = casted[nIndex + 2];
+					var g = casted[nIndex + 1];
+					var b = casted[nIndex];
+					var a = 255;
+					// imagedata.data[nIndex] = r;
+					// imagedata.data[nIndex+1] = g;
+					// imagedata.data[nIndex+2] = b;
+					// imagedata.data[nIndex+3] = a;
+					
+					normalArr.push(r);
+					normalArr.push(g);
+					normalArr.push(b);
+					normalArr.push(a);
+					
+					////console.log(nRow, nCol, [r, g, b]);
+					
 					//break;
 				}
-				//console.info('normalArr:', normalArr.toString());
-				*/
-				imagedata.data.set(casted);
+				////console.info('normalArr:', normalArr.toString());
+				imagedata.data.set(normalArr);
 				console.timeEnd('normal way');
 				// normal way
-				
-				/*
-				// uint32 way
-				console.time('uint32 way');
-				var buf = new ArrayBuffer(w * h * 4);
-				var buf8 = new Uint8ClampedArray(buf);
-				var buf32 = new Uint32Array(buf);
-				
-				for (var nRow=0; nRow<h; ++nRow) {
-					for (var nCol=0; nCol<w; ++nCol) {
-						var rez_byte = ostypes.API('GetPixel')(hdcC, nCol, nRow);
-						var input = parseInt(cutils.jscGetDeepest(rez_byte));
-						
-						buf32[nRow * w + nCol] = input | (255 << 24); //alpha of 255 otherwise it is 0
-						//break;
-					}
-					//break;
-				}
-				//console.info('normalArr:', normalArr.toString());
-				imagedata.data.set(buf8);
-				console.timeEnd('uint32 way');
-				// uint32 way
-				*/
-				
+				//*/
 				return imagedata;
 				
 			break;
