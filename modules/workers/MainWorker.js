d1f16296bb2d5732a43852789d7e845421d675ff
Noitidart <noitidart@gmail.com>
2015-05-10 16:21:04 -0700

    ok getting passed that big thing now

--- modules/workers/MainWorker.js
+++ modules/workers/MainWorker.js
@@ -541,8 +541,6 @@ function shootMon(mons) {
 						});
 					}
 					
-					return []; // debug
-					
 					// NSGraphicsContext* context = [NSGraphicsContext graphicsContextWithBitmapImageRep:imageRep];
 					var NSGraphicsContext = ostypes.HELPER.class('NSGraphicsContext');
 					var context = ostypes.API('objc_msgSend')(NSGraphicsContext, ostypes.HELPER.sel('graphicsContextWithBitmapImageRep:'), imageRep);
