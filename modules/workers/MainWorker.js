e640a88cb4be5f3c468ae590b4d5b180ade898b1
Noitidart <noitidart@gmail.com>
2015-05-10 22:18:35 -0700

    looking crash point

--- modules/workers/MainWorker.js
+++ modules/workers/MainWorker.js
@@ -636,6 +636,8 @@ function shootMon(mons) {
 					console.info('data:', data.toString());
 					
 					// [data writeToFile:@"/tmp/screenshot.png" atomically:YES];
+					console.info(OS.Path.join(OS.Constants.Path.desktopDir, 'full_ss.png'));
+					console.info(myNSStrings.get(OS.Path.join(OS.Constants.Path.desktopDir, 'full_ss.png')));
 					//var rez_writeToFile = ostypes.API('objc_msgSend')(data, ostypes.HELPER.sel('writeTofile:atomically:'), myNSStrings.get(OS.Path.join(OS.Constants.Path.desktopDir, 'full_ss.png')), ostypes.CONST.YES);
 					//console.info('rez_writeToFile:', rez_writeToFile.toString(), uneval(rez_writeToFile));
 					
