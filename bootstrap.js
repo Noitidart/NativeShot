f54a5b6e15c91b5097d7fdef4cd53b579e606e3c
Noitidart <noitidart@gmail.com>
2015-07-11 05:03:35 -0700

    1.0-night.rev25
    ### bootstrap.js & panel.xul & canvas_bg.png
    * Drawing to the canvas
    * Using that transparent photoshop background pattern
    * Made so panel.xul comes up with opacity same as that I draw over the
    canvas so it looks more responsive

--- bootstrap.js
+++ bootstrap.js
@@ -153,11 +153,22 @@ function takeShot(aDOMWin) {
 		
 		
 		console.error('topLeftMostX:', topLeftMostX, 'topLeftMostY:', topLeftMostY, 'fullWidth:', fullWidth, 'fullHeight:', fullHeight, '_END_');
-		var panel = Services.ww.openWindow(null, core.addon.path.content + "panel.xul", "_blank", "chrome,width=" + fullWidth + ",height=" + fullHeight + ",screenX=" + topLeftMostX + ",screenY=" + topLeftMostY, null);
+		var panel = Services.ww.openWindow(null, core.addon.path.content + 'panel.xul', '_blank', 'chrome,alwaysRaised,width=' + fullWidth + ',height=' + fullHeight + ',screenX=' + topLeftMostX + ',screenY=' + topLeftMostY, null);
 		console.info('panel:', panel);
 		panel.addEventListener('load', function() {
 			console.error('yeaaa loaddded');
+			
 			panel.moveTo(topLeftMostX, topLeftMostY); // i cant set left and top off screen in openWindow because per docs from https://developer.mozilla.org/en-US/docs/Web/API/Window/open#Note_on_position_and_dimension_error_correction ==> "Note on position and dimension error correction Requested position and requested dimension values in the features list will not be honored and will be corrected if any of such requested value does not allow the entire browser window to be rendered within the work area for applications of the user's operating system. No part of the new window can be initially positioned offscreen. This is by default in all Mozilla-based browser releases."
+			
+			/* // was thinking of using this to prevent the window from adding to taskbar, but i then realized its not big deal as ill have a solid panel over it, so they wont ever see the extra window in taskbar
+			var xulwin = panel.QueryInterface(Ci.nsIInterfaceRequestor)
+							.getInterface(Ci.nsIWebNavigation)
+							.QueryInterface(Ci.nsIDocShellTreeItem)
+							.treeOwner
+							.QueryInterface(Ci.nsIInterfaceRequestor)
+							.getInterface(Ci.nsIXULWindow);
+			Services.appShell.unregisterTopLevelWindow(xulwin);
+			*/
 			
 			var win = panel;
 			var doc = panel.document;
@@ -165,11 +176,15 @@ function takeShot(aDOMWin) {
 			var can = doc.createElementNS(NS_HTML, 'canvas');
 			can.width = fullWidth; // just a note from left over stuff, i can do aVal[s].idat.width now but this tells me some stuff: cannot do `aVal.width` because DIB widths are by 4's so it might have padding, so have to use real width
 			can.height = fullHeight;
+			can.style.background = '#000 url(' + core.addon.path.images + 'canvas_bg.png) repeat fixed top left'
 			var ctx = can.getContext('2d');
 			
 			for (var s=0; s<aVal.length; s++) {
-				//ctx.putImageData(aVal[s].idat, aVal[s].xTopLeft + Math.abs(topLeftMostX), aVal[s].yTopLeft + Math.abs(topLeftMostY));
+				ctx.putImageData(aVal[s].idat, aVal[s].xTopLeft + Math.abs(topLeftMostX), aVal[s].yTopLeft + Math.abs(topLeftMostY));
 			}
+			
+			ctx.fillStyle = 'rgba(0,0,0,.6)';
+			ctx.fillRect(0, 0, fullWidth, fullHeight);
 			
 			doc.documentElement.appendChild(can);
 
