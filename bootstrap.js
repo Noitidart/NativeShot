8195dc11929fe96a5ef38b6b77d8e579a89d93bf
Noitidart <noitidart@gmail.com>
2015-07-11 14:18:27 -0700

    osx issue after that so trying this for osx

--- bootstrap.js
+++ bootstrap.js
@@ -165,6 +165,7 @@ function takeShot(aDOMWin) {
 			if (core.os.name == 'drawin') {
 				panel.setTimeout(function() {
 					panel.resizeTo(fullWidth, fullHeight);
+					panel.moveTo(topLeftMostX, topLeftMostY);
 				}, 10);
 			} else {
 				panel.resizeTo(fullWidth, fullHeight);
