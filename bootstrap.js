72aa579e46406ed679f99bce752aee823b23b243
Noitidart <noitidart@gmail.com>
2015-07-12 06:47:38 -0700

    whaaa?? not even from main?

--- bootstrap.js
+++ bootstrap.js
@@ -194,7 +194,9 @@ function obsHandler_nativeshotEditorLoaded(aSubject, aTopic, aData) {
 							function(aVal) {
 								console.log('Fullfilled - promise_makeWinFullAllMon - ', aVal);
 								// start - do stuff here - promise_makeWinFullAllMon
-								aEditorDOMWindow.fullscreen = true;
+								setTimeout(function() {
+									aEditorDOMWindow.fullscreen = true;
+								}, 1000);
 								// end - do stuff here - promise_makeWinFullAllMon
 							},
 							function(aReason) {
