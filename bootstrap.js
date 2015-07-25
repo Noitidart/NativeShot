1c1da043a2a25bff34c71282d0b6d0677d0f3db6
Noitidart <noitidart@gmail.com>
2015-07-25 02:15:34 -0700

    no opener modding

--- bootstrap.js
+++ bootstrap.js
@@ -1352,7 +1352,7 @@ var windowListener = {
 				// as whenever i print from my hidden frame on link678321212 it opens the print dialog with opener set to null, and then it tries opener.focus() and it then leaves the window open
 				// :todo: i should maybe target specifically my printer window, as if other people open up with opener null then i dont know if i should fix for them from here, but right now it is, and if opener ever is null then they'll run into that problem of window not closing (at least for me as tested on win81)
 				console.error('going to set opener to wm! as it was null');
-				aDOMWindow.opener = Services.wm.getMostRecentWindow(null); // { focus: function() { } };
+				//aDOMWindow.opener = Services.wm.getMostRecentWindow(null); // { focus: function() { } };
 				//console.error('ok set opener! it is:', aDOMWindow.opener);
 			}
 		}
