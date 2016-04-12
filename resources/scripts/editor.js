b535493dc47157d7908922d2ad9fd1846ab71655
Noitidart <noitidart@gmail.com>
2016-04-11 22:47:27 -0700

    modified comm method b/w editor and bootstrap

--- resources/scripts/editor.js
+++ resources/scripts/editor.js
@@ -1,5 +1,12 @@
 Components.utils.import('resource://gre/modules/Services.jsm');
-// var gIMon = window.arguments[0];
+
+var core = {
+	addon: {
+		name: 'NativeShot',
+		id: 'NativeShot@jetpack'
+	}
+};
+
 var gQS = queryStringAsJson(window.location.search.substr(1));
 console.log('gQS:', gQS, window.location.search.substr(1));
 
@@ -63,7 +70,10 @@ function init() {
 	gCtxDim.fillRect(0, 0, gQS.w, gQS.h);
 	console.log('filled:', 0, 0, gQS.w, gQS.h)
 	// }, 1000);
-	Services.obs.notifyObservers(null, 'NativeShot@jetpack_nativeshot-editor-loaded', gQS.iMon);
+	Services.obs.notifyObservers(null, core.addon.id + '_nativeshot-editor-request', JSON.stringify({
+		topic: 'init',
+		iMon: gQS.iMon
+	}));
 }
 
 function screenshotXfer(aData) {
@@ -111,68 +121,4 @@ function queryStringAsJson(aQueryString) {
 	asJsonStringify = asJsonStringify.replace(/"(\d+(?:.\d+)?|true|false)"/g, function($0, $1) { return $1; });
 	
 	return JSON.parse(asJsonStringify);
-}
-
-function jsonToDOM(json, doc, nodes) {
-
-    var namespaces = {
-        html: 'http://www.w3.org/1999/xhtml',
-        xul: 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul'
-    };
-    var defaultNamespace = namespaces.html;
-
-    function namespace(name) {
-        var m = /^(?:(.*):)?(.*)$/.exec(name);        
-        return [namespaces[m[1]], m[2]];
-    }
-
-    function tag(name, attr) {
-        if (Array.isArray(name)) {
-            var frag = doc.createDocumentFragment();
-            Array.forEach(arguments, function (arg) {
-                if (!Array.isArray(arg[0]))
-                    frag.appendChild(tag.apply(null, arg));
-                else
-                    arg.forEach(function (arg) {
-                        frag.appendChild(tag.apply(null, arg));
-                    });
-            });
-            return frag;
-        }
-
-        var args = Array.slice(arguments, 2);
-        var vals = namespace(name);
-        var elem = doc.createElementNS(vals[0] || defaultNamespace, vals[1]);
-
-        for (var key in attr) {
-            var val = attr[key];
-            if (nodes && key == 'id')
-                nodes[val] = elem;
-
-            vals = namespace(key);
-            if (typeof val == 'function')
-                elem.addEventListener(key.replace(/^on/, ''), val, false);
-            else
-                elem.setAttributeNS(vals[0] || '', vals[1], val);
-        }
-        args.forEach(function(e) {
-            try {
-                elem.appendChild(
-                                    Object.prototype.toString.call(e) == '[object Array]'
-                                    ?
-                                        tag.apply(null, e)
-                                    :
-                                        e instanceof doc.defaultView.Node
-                                        ?
-                                            e
-                                        :
-                                            doc.createTextNode(e)
-                                );
-            } catch (ex) {
-                elem.appendChild(doc.createTextNode(ex));
-            }
-        });
-        return elem;
-    }
-    return tag.apply(null, json);
 }
