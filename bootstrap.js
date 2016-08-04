03d136eea303437f0d266ccf8f534ee177bcd29e
Noitidart <noitidart@gmail.com>
2016-08-04 10:44:48 -0700

    1.9b.ev74
    * The addon setting applyBackgroundUpdates was being set to a string
    when set from inline options page, so I'm parseInt'ing it now

--- bootstrap.js
+++ bootstrap.js
@@ -1119,7 +1119,7 @@ function getAddonInfo(aAddonId=core.addon.id) {
 	var deferredmain_getaddoninfo = new Deferred();
 	AddonManager.getAddonByID(aAddonId, addon =>
 		deferredmain_getaddoninfo.resolve({
-			applyBackgroundUpdates: addon.applyBackgroundUpdates === 1 ? (AddonManager.autoUpdateDefault ? 2 : 0) : addon.applyBackgroundUpdates,
+			applyBackgroundUpdates: parseInt(addon.applyBackgroundUpdates) === 1 ? (parseInt(AddonManager.autoUpdateDefault) ? 2 : 0) : parseInt(addon.applyBackgroundUpdates),
 			updateDate: addon.updateDate.getTime(),
 			version: addon.version
 		})
