418b74679159ca8a87a1173e848b88cc7f287d47
Noitidart <noitidart@gmail.com>
2017-01-25 20:25:30 -0800

    child_process pinging working on windows!

--- src/electron/main.js
+++ src/electron/main.js
@@ -2,78 +2,103 @@ const {app, Menu, Tray, BrowserWindow} = require('electron');
 const path = require('path')
 const url = require('url')
 
-let win
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+var win;
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+    // Open the DevTools.
+    win.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+    });
 }
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+function startTray() {
+    // run in app.on ready - i dont know why but @wadie did it like this
+    var platform = require('os').platform();
+    let appIcon =  new Tray('icon.' + (platform == 'win32' ? 'ico' : 'png'));
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Screenshots',
+            type: 'radio'
+        }
+    ]);
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
+    // Make a change to the context menu
+    contextMenu.items[0].checked = false;
 
-let appIcon = null
-var platform = require('os').platform();
-app.on('ready', () => {
-if (platform == 'win32') {
-    appIcon = new Tray('icon.ico');
+    // Call this again for Linux because we modified the context menu
+    appIcon.setContextMenu(contextMenu);
 }
-else {
-  appIcon = new Tray('icon.png');
+
+const { TextEncoder, TextDecoder} = require('./node_modules/text-encoding/index.js');
+function startChildProc() {
+    // run in app.on ready - because i need TextEncoder which comes in from the require statement
+    var spawn = require('child_process').spawn,
+        child = spawn('./nativeshot.exe');
+
+    // child.stdin.setEncoding('utf-8');
+    child.stdout.pipe(process.stdout);
+
+    console.log('Hey there');
+    var message = "\"ping\"";
+
+    // https://dxr.mozilla.org/mozilla-central/source/toolkit/components/extensions/NativeMessaging.jsm#252
+    var buf = new Buffer(new TextEncoder().encode(message).buffer);
+    console.log('buf:', buf);
+
+    // https://dxr.mozilla.org/mozilla-central/source/toolkit/components/extensions/NativeMessaging.jsm#298
+    var sizebuf = new Buffer(Uint32Array.of(buf.byteLength).buffer);
+    console.log('sizebuf:', sizebuf);
+
+    child.stdin.write(sizebuf);
+    child.stdin.write(buf);
+    // child.stdin.end();
+
+    setTimeout(function() {
+        console.log('ok will send ping again');
+        child.stdin.write(sizebuf);
+        child.stdin.write(buf);
+        // child.stdin.end();
+    }, 5000);
 }
-  const contextMenu = Menu.buildFromTemplate([
-    {label: 'Screenshots', type: 'radio'}
-  ])
 
-  // Make a change to the context menu
-  contextMenu.items[0].checked = false
+function readyHandler() {
+    startTray();
+    startChildProc();
+    createWindow();
+}
 
-  // Call this again for Linux because we modified the context menu
-  appIcon.setContextMenu(contextMenu);
-})
+function activateHandler() {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createWindow();
+    }
+}
 
-var spawn = require('child_process').spawn,
-    child = spawn('./nativeshot');
+function allwinClosedHandler() {
+    // Quit when all windows are closed.
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+}
 
-child.stdin.setEncoding('utf-8');
-child.stdout.pipe(process.stdout);
-
-console.log('Hey there');
-child.stdin.write("\"ping\"");
-
-child.stdin.end();
+app.on('ready', readyHandler);
+app.on('activate', activateHandler);
+app.on('window-all-closed', allwinClosedHandler);
